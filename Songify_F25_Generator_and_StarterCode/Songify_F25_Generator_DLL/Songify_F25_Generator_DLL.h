5eb0a1abb2ef8696c6c2d6669d5f71d3df1da9e6
michaelfeeney <michaelwcfeeney@gmail.com>
2025-11-03 08:44:26 -0500

    Updated project #1 due date

---
+++ Songify_F25_Generator_and_StarterCode/Songify_F25_Generator_DLL/Songify_F25_Generator_DLL.h
@@ -0,0 +1,15 @@
+#ifndef _Songify_F25_Generator_DLL_HG_
+#define _Songify_F25_Generator_DLL_HG_
+
+// Michael Feeney, 2025
+// mfeeney (at) fanshawec.ca
+//
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+
+#include "cPersonGenerator.h"
+#include "cMusicGenerator.h"
+
+
+#endif // Songify_F25_Generator_Library
